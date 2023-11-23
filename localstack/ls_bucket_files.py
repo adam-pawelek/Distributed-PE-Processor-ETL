1f70d778008c0fc3542f11ad09ff3626049b8cd7
adam <adam.wk@outlook.com>
2023-11-23 22:45:32 +0100

    update ls_bucket_files.py

--- localstack/ls_bucket_files.py
+++ localstack/ls_bucket_files.py
@@ -1,3 +1,5 @@
+import json
+
 import boto3
 
 BUCKET_NAME = "my-bucket"
@@ -5,6 +7,23 @@ FILE_NAME = "myfile.txt"
 ENDPOINT_URL = "http://localhost:4566"
 
 s3_client = boto3.client('s3', endpoint_url=ENDPOINT_URL)
+
+# Create a bucket
+s3_client.create_bucket(Bucket=BUCKET_NAME)
+
+# Make the bucket public
+bucket_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddPublicReadAccess",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": f"arn:aws:s3:::{BUCKET_NAME}/*"
+        }
+    ]
+}
 
 # Download a file from the bucket
 s3_client.download_file(BUCKET_NAME, FILE_NAME, FILE_NAME)
